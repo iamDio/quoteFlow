f5e3bf7799d89fd2e15c8460fe6a947be5aed249
diodev2 <19352823+iamDio@users.noreply.github.com>
2025-08-05 23:53:39 -0400

    feat: setup redis for caching and session mgmt

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -6,9 +6,19 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { QuoteModule } from './quote/quote/quote.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager';
 
 @Module({
   imports: [
+    CacheModule.registerAsync({
+      useFactory: () => ({
+        store: redisStore,
+        host: 'localhost',
+        port: 6379,
+        ttl: 60, // default cache TTL in seconds
+      }),
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'apps/backend/src/schema.gql'),
--- apps/server/src/quote/quote/quote.service.ts
+++ apps/server/src/quote/quote/quote.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateQuoteInput } from '../dto/quote.input';
 import { Quote } from '../dto/quote.output';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 
 @Injectable()
 export class QuoteService {
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
+  async getQuote(id: string) {
+    const cacheKey = `quote:${id}`;
+    const cached = await this.cacheManager.get(cacheKey);
+    if (cached) {
+      return cached;
+    }
+    const quote = await this.findQuoteInDb(id); // however you're doing it
+    await this.cacheManager.set(cacheKey, quote, 60); // 60s TTL
+
+    return quote;
+  }
+
+  private findQuoteInDb(id: string) {
+    return {
+      id,
+      plan: 'Premium',
+      monthlyPremium: 120.5,
+      coverage: 'full',
+    };
+  }
+
   createQuote(input: CreateQuoteInput): Quote {
     return {
       monthlyPremium: 66,
